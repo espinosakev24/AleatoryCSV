8846b993932a3d49b3b25b6e8d41aac9896a71be
espinosakev24 <kevinespinosazapata@gmail.com>
2020-06-12 14:45:16 -0500

    adds fields aleatory generation

--- modules.py
+++ modules.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import random
 import requests
+import datetime
 
 
 def randomTitle():
@@ -14,3 +15,35 @@ def randomDescription():
     randomIdx = random.randint(0, 20)
     randomDescription = jsonArr[randomIdx]["body"]
     return randomDescription
+
+def randomPriority():
+    prioritiesList = [
+        "Open", "Waiting on Client", "In Progress",
+        "Defer", "Resolved/Fixed", "QA Verified", "",
+        "Won’t Fix", "Duplicate", "User Error", "Reopen",
+        "QA Verify"
+    ]
+    return prioritiesList[random.randint(0, len(prioritiesList) - 1)]
+
+def randomType():
+    typeList = [
+        "Bug", "Enhancement", "Feature", "Task", "",
+    ]
+    return typeList[random.randint(0, len(typeList) - 1)]
+print(randomType())
+
+def randomOS():
+    oslist = ["Windows 10", "MAC", "Linux", "Ubuntu"]
+    return oslist[random.randint(0, len(oslist) - 1)]
+
+def randomBrowserName():
+    browserlist = ["Opera", "Chrome 1.2", "Firefox 3.4.2", "Explorer 8"]
+
+def randomduedate():
+    start_date = datetime.date(2016, 1, 1)
+    end_date = datetime.date(2021, 2, 1)
+    time_between_dates = end_date - start_date
+    days_between_dates = time_between_dates.days
+    random_number_of_days = random.randrange(days_between_dates)
+    random_date = start_date + datetime.timedelta(days=random_number_of_days)
+    return random_date.strftime("%m/%d/%Y")
